6da63d83561fd4fb6d6635847feb046c4d8fcec7
mikiones <mikiones@localhost>
2008-06-19 22:25:36 +0000

    some db character list staff ended

--- java-muonline-server-files/src/net/sf/jmuserver/gs/database/MuCharacterListDB.java
+++ java-muonline-server-files/src/net/sf/jmuserver/gs/database/MuCharacterListDB.java
@@ -16,17 +16,69 @@ import java.util.logging.Logger;
  */
 public class MuCharacterListDB {
 
+    public class BasicStat {
+
+        public int _agi;
+        public int _str;
+        public int _vit;
+        public int _ene;
+        public int _com;
+    }
+    //user id in database
     private int _userId = 0;
+    //connection to database
+    java.sql.Connection con = null;
 
     public MuCharacterListDB(int UserIdInDatabase) {
         _userId = UserIdInDatabase;
+        try {
+            con = MuDataBaseFactory.getInstance().getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(MuCharacterListDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
-    public boolean itsNickAvailble(String name) {
+    /**
+     * get the template stats fo new character
+     * @param classCode 
+     * @return
+     */
+    private BasicStat getBaseStats(byte classCode) {
+        BasicStat t = new BasicStat();
+        try {
+
+
+
+            PreparedStatement statement = con.prepareStatement("SELECT *  from chatacter_base_stats where ch_base_class =" + classCode);
+
+            ResultSet rset = statement.executeQuery();
+            if (!rset.next()) {
+                System.out.println("Wrong CharCode");
+                return null; //  
+
+            }
+            t._agi = rset.getInt("ch_base_agi");
+            t._str = rset.getInt("ch_base_str");
+            t._vit = rset.getInt("ch_base_vit");
+            t._ene = rset.getInt("ch_base_enr");
+            t._com = rset.getInt("ch_base_com");
+
+
+        } catch (SQLException ex) {
+            Logger.getLogger(MuCharacterListDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return t;
+    }
+
+    /**
+     * check avalible nick
+     * @param name
+     * @return
+     */
+    private boolean itsNickAvailble(String name) {
         boolean success = false;
         try {
-            java.sql.Connection con = null;
-            con = MuDataBaseFactory.getInstance().getConnection();
+
             PreparedStatement statement = con.prepareStatement("select*  from " + MuCharactersDb.CH_TAB + " where " + MuCharactersDb.CH_NAME + " = '" + name + "' ");
             ResultSet rset = statement.executeQuery();
             try {
@@ -40,14 +92,18 @@ public class MuCharacterListDB {
         return success;
     }
 
-    public int SpaceAvailbele() {
+    /**
+     * getspaces availbele
+     * @return
+     */
+    private int getSpaceAvailbele() {
         int spaces = 0;
         try {
-            java.sql.Connection con = null;
-            con = MuDataBaseFactory.getInstance().getConnection();
+
             PreparedStatement statement = con.prepareStatement("SELECT u_ch_c FROM users where u_id =" + _userId);
             ResultSet rset = statement.executeQuery();
-            spaces = 5 - rset.getInt("u_ch_c");
+            rset.next();
+            spaces = rset.getInt("u_ch_c");
 
 
         } catch (SQLException ex) {
@@ -56,16 +112,54 @@ public class MuCharacterListDB {
         return spaces;
     }
 
+    /**
+     * added new character todatabase 
+     * @param name
+     * @param classCode
+     * @return true when done
+     */
     public boolean addNewCharacter(String name, byte classCode) {
         boolean success = false;
+        PreparedStatement statement;
         //firs check aailible name
         if (!itsNickAvailble(name)) {
-            int spaces = SpaceAvailbele();
-            if (spaces >= 1) {
-                
+            int spaces = getSpaceAvailbele();
+            if (spaces < 5) {
+                BasicStat s = getBaseStats(classCode);
+                if (s != null) {
+                    try {
+                        statement = con.prepareStatement("INSERT INTO characters(" + "us_id" + ", ch_name" + ", ch_class" + ", ch_stat_lvl" + ", ch_stat_str" + ", ch_stat_agi" + ", ch_stat_vit" + ", ch_stat_enr," + " ch_stat_com," + " ch_last_pos_map," + " ch_last_pos_h, " + " ch_exp_act, " + "ch_exp_lp, " + "ch_last_pos_x, ch_last_pos_y)    VALUES (?, ?, ?, ?, ?, ?,             ?, ?, ?, ?, ?,             ?, ?, ?, ?);");
+                        statement.setInt(1, _userId);
+                        statement.setString(2, name);
+                        statement.setInt(3, classCode);
+                        statement.setInt(4, 1);//lvl
+                        statement.setInt(5, s._str);
+                        statement.setInt(6, s._agi);
+                        statement.setInt(7, s._vit);
+                        statement.setInt(8, s._ene);
+                        statement.setInt(9, s._com);
+                        statement.setInt(10, 0); //lorencia
+                        statement.setInt(11, 0);//flag
+                        statement.setInt(12, 0);//exp
+                        statement.setInt(13, 0);//lp
+                        statement.setInt(14, 128);//x pos
+                        statement.setInt(15, 128);//y pos
+                        ResultSet rset = statement.executeQuery();
+                        if (rset.next())
+                        {
+                            statement=con.prepareStatement("UPDATE users SET  u_ch_c=? WHERE u_id=?");
+                                    statement.setInt(1, _userId);
+                                    statement.setInt(2, spaces+1);
+                                    ResultSet rset1 = statement.executeQuery();
+                                    if(rset1.next())
+                                        success=true;
+                        }
+                    } catch (SQLException ex) {
+                        Logger.getLogger(MuCharacterListDB.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
             }
-            }
-            return success;
         }
-
+        return success;
     }
+}
