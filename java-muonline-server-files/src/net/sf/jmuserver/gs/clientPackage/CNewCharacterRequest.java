68ce97188b1a08c435a47d68acb2088c69303018
marcelghe87 <marcelghe87@localhost>
2008-06-19 20:14:57 +0000

    Saves chars with the right class id. Saves char in CharList.

--- java-muonline-server-files/src/net/sf/jmuserver/gs/clientPackage/CNewCharacterRequest.java
+++ java-muonline-server-files/src/net/sf/jmuserver/gs/clientPackage/CNewCharacterRequest.java
@@ -10,18 +10,23 @@ import net.sf.jmuserver.gs.serverPackage.SNewCharacterAnsfer;
 public class CNewCharacterRequest extends ClientBasePacket {
 
 	private String _name;
-	private short _class;
+	private int _class;
 	public CNewCharacterRequest(byte[] decrypt, ClientThread _client) throws IOException, Throwable {
             super(decrypt);
             _name = readS(2, 10);
             _class=decrypt[12];
+            _class = _class * 2;
             System.out.println(decrypt.length);
             System.out.println("Create Character '"+_name+ "' Reuest class = "+_class);
-            MuCharacterBase newCB = new  MuCharacterBase(_name,1,_class,5,new MuCharacterWear());
+            
+            int position = _client.getChList().getCharsCount();   
+            
+            MuCharacterBase newCB = new  MuCharacterBase(_name,1,_class,position,new MuCharacterWear());
             _client.getChList().addNew(newCB);
-            // TODO: Save char in DB, and pass result to SNewCharacterAnsfer (2nd param)
-            // (use stored proc add_new_character())
-            _client.getConnection().sendPacket(new SNewCharacterAnsfer(newCB, (short)1));
+            
+            boolean success = _client.storeNewChar(_client.getUser().getId(), _name, _class);
+            
+            _client.getConnection().sendPacket(new SNewCharacterAnsfer(newCB, success,position));
         }
 
 	@Override
