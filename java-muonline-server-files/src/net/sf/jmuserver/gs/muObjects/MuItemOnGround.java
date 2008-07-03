0b7dd528207e02903a9991c4ad221e187a2c1d4f
mikiones <mikiones@localhost>
2008-07-03 14:13:54 +0000

    ItemOnGround SOme updates

--- java-muonline-server-files/src/net/sf/jmuserver/gs/muObjects/MuItemOnGround.java
+++ java-muonline-server-files/src/net/sf/jmuserver/gs/muObjects/MuItemOnGround.java
@@ -1,5 +1,19 @@
 package net.sf.jmuserver.gs.muObjects;
 
-public class MuItemOnGround  extends MuObject{
-//private MuItemHex _item;
+import net.sf.jmuserver.gs.templates.MuItemT;
+
+public class MuItemOnGround extends MuObject {
+
+    private MuItemT _itemHex;
+
+    public MuItemT getItem() {
+        return _itemHex;
+    }
+
+    public MuItemOnGround() {
+        //sheld :P
+        _itemHex=new MuItemT(new byte[]{(byte)0xc0, (byte) 0x00, (byte) 0x16, (byte) 0x00, (byte) 0x00});
+        
+    }
+    
 }
