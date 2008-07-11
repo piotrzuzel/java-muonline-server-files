b55091d4fc54cc5590449e398062a722448f8c95
mikiones <mikiones@localhost>
2008-07-11 17:32:18 +0000

    added to info "name" of obiect kind mucharacter

--- java-muonline-server-files/src/net/sf/jmuserver/gs/muObjects/MuMap.java
+++ java-muonline-server-files/src/net/sf/jmuserver/gs/muObjects/MuMap.java
@@ -13,22 +13,25 @@ import net.sf.jmuserver.gs.muObjects.MuMap.MuMapPoint;
  * @version $Revision: 1.3 $ $Date: 2004/11/02 14:33:16 $
  */
 public class MuMap {
-/**
- * MOvung to new position Update Maps
- * @param Who who move
- * @param x new x pos
- * @param y new y os
- * @return true when done
- */
+
+    /**
+     * MOvung to new position Update Maps
+     * @param Who who move
+     * @param x new x pos
+     * @param y new y os
+     * @return true when done
+     */
     boolean MoveTo(MuCharacter Who, int x, int y) {
-        int actX=Who.getX();
-        int actY=Who.getY();
-        int x1=actX/5;
-        int y1=actY/5;
-        int x2=x/5;
-        int y2=y/5;
+        int actX = Who.getX();
+        int actY = Who.getY();
+        int x1 = actX / 5;
+        int y1 = actY / 5;
+        int x2 = x / 5;
+        int y2 = y / 5;
         //if there onthis some point
-        if((x1==x2)&&(y1==y2)) return true;
+        if ((x1 == x2) && (y1 == y2)) {
+            return true;
+        }
         _mapa[x1][y1].punkt.remove(Who);
         _mapa[x2][y2].punkt.add(Who);
         return true;
@@ -76,8 +79,8 @@ public class MuMap {
 
             punkt.remove(ss);
         }
-        public void PrintData()
-        {
+
+        public void PrintData() {
             for (Iterator<MuObject> it = punkt.iterator(); it.hasNext();) {
                 MuObject muObject = it.next();
                 System.out.println(muObject);
@@ -95,14 +98,15 @@ public class MuMap {
      * all Obiect visitable on map
      */
     private Map<Integer, MuObject> _visibleObjects;
-/**
- * init map
- * @param m map byte code
- * @param MapName name of map
- */
-    public MuMap(int m,String MapName) {
+
+    /**
+     * init map
+     * @param m map byte code
+     * @param MapName name of map
+     */
+    public MuMap(int m, String MapName) {
         _mapCode = (byte) m;
-        _mapName=MapName;
+        _mapName = MapName;
         _allPlayers = new HashMap<String, MuObject>();
         _visibleObjects = new HashMap<Integer, MuObject>();
         for (int i = 0; i <= 50; i++) {
@@ -118,26 +122,28 @@ public class MuMap {
      */
     public void addVisibleObject(MuObject object) {
         //add to basic  list
-        System.out.println("|Adding New Visibable obiect to map:"+_mapName);
+        System.out.println("|Adding New Visibable obiect to map:" + _mapName);
         System.out.println("|--ObiectId [" + object.getObjectId() + "].");
         _visibleObjects.put(new Integer(object.getObjectId()), object);
         //add the obiect to specific  placyinmeet optionalise list
         int x = object.getX() / 5;
         int y = object.getY() / 5;
         if (_mapa[x][y] == null) {
-            System.out.println("|--MuPoint [" + x + ","+y+"] not create new... Done");
+            System.out.println("|--MuPoint [" + x + "," + y + "] not create new... Done");
             _mapa[x][y] = new MuMapPoint();
         }
         _mapa[x][y].add(object);
         System.out.println("|--Wsp [" + object.getX() + "," + object.getY() + "] At PointMap [" + x + "," + y + "].");
-
+        if (object instanceof MuCharacter) {
+        System.out.println("|--Obieect is MuCharater kind  Name:[" + ((MuCharacter) object).getName() + "].");
+        }
         if (object instanceof MuPcInstance) {
             System.out.println("|--Obieect is PcInstance  Name:[" + ((MuPcInstance) object).getName() + "].");
             _allPlayers.put(((MuPcInstance) object).getName().toLowerCase(),
                     object);
         }
         System.out.println("|______________________________________");
-        
+
     }
 
     /**
@@ -146,8 +152,8 @@ public class MuMap {
      */
     public void removeVisibleObject(MuObject object) {
         _visibleObjects.remove(new Integer(object.getObjectId()));
-        int x = object.getX()/5;
-        int y = object.getY()/5;
+        int x = object.getX() / 5;
+        int y = object.getY() / 5;
         //if (_mapa[x][y].conteins(object)) 
         //{
         _mapa[x][y].removeID(object.getObjectId());
@@ -173,24 +179,33 @@ public class MuMap {
     /**
      * @return Colection of  9'th pointson map 
      */
-    private Collection<MuObject> GetObiectsFrom9ts(int x,int y)
-    {
-        
-        Collection<MuObject> t = new Vector<MuObject>() ;
-        int x1 = x-1;
-        int x2 = x+1;
-        int y1 = y-1;
-        int y2 = y+1;
-        if(x1<0)x1=0;
-        if(x2>50)x2=50;
-        if(y1<0)y1=0;
-        if(y2>50)y2=0;
-        for(int i =x1;i<=x2;i++)
-            for(int j=y1;j<=y2;j++){
+    private Collection<MuObject> GetObiectsFrom9ts(int x, int y) {
+
+        Collection<MuObject> t = new Vector<MuObject>();
+        int x1 = x - 1;
+        int x2 = x + 1;
+        int y1 = y - 1;
+        int y2 = y + 1;
+        if (x1 < 0) {
+            x1 = 0;
+        }
+        if (x2 > 50) {
+            x2 = 50;
+        }
+        if (y1 < 0) {
+            y1 = 0;
+        }
+        if (y2 > 50) {
+            y2 = 0;
+        }
+        for (int i = x1; i <= x2; i++) {
+            for (int j = y1; j <= y2; j++) {
                 t.addAll(_mapa[i][j].punkt);
-            }    
+            }
+        }
         return t;
     }
+
     /**
      * getting all visitable  obiect for obiect
      * @param object
@@ -198,29 +213,30 @@ public class MuMap {
      */
     public Vector<MuObject> getVisibleObjects(MuObject object) {
         //Vector t = new Vector();
-        
-        Collection t =GetObiectsFrom9ts(object.getX()/5, object.getY()/5);
+
+        Collection t = GetObiectsFrom9ts(object.getX() / 5, object.getY() / 5);
         //remove myslf
-        t.remove(object); 
+        t.remove(object);
         return new Vector<MuObject>(t);
     }
-    
+
     /**
      * get all Plyers Nearto Obiect
      * @param object
      * @return Vector of players
      */
-    public Vector getVisiblePlayers(MuObject object)
-    {
-        Vector<MuPcInstance> _list=new Vector<MuPcInstance>();
+    public Vector getVisiblePlayers(MuObject object) {
+        Vector<MuPcInstance> _list = new Vector<MuPcInstance>();
         Collection t = getVisibleObjects(object);
         for (Iterator it = t.iterator(); it.hasNext();) {
             Object object1 = it.next();
-            if(object1  instanceof MuPcInstance) _list.add((MuPcInstance) object1);
+            if (object1 instanceof MuPcInstance) {
+                _list.add((MuPcInstance) object1);
+            }
         }
         return _list;
     }
-    
+
     /**
      * get al visitable obiects
      * @return
