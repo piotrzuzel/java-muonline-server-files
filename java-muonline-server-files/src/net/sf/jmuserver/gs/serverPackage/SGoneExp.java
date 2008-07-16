39a9ec61e58c9d0b9bd09dd53459dd7b74baf195
mikiones <mikiones@localhost>
2008-07-16 12:58:07 +0000

    Protocol GetExperance

--- java-muonline-server-files/src/net/sf/jmuserver/gs/serverPackage/SGoneExp.java
+++ java-muonline-server-files/src/net/sf/jmuserver/gs/serverPackage/SGoneExp.java
@@ -2,20 +2,35 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package net.sf.jmuserver.gs.serverPackage;
 
 import java.io.IOException;
 
 /**
- *
+ * Protocol:GetExpFromId c3 16
  * @author Miki i Linka
  */
-public class SGoneExp extends ServerBasePacket{
+public class SGoneExp extends ServerBasePacket {
+
+    private short _id;
+    private int _exp;
+
+    /**
+     * send  packet iGetExp :
+     * @param IdOfKilledMonster which we gt exp reward
+     * @param ExpReward how exp we get
+     */
+    public SGoneExp(int IdOfKilledMonster, int ExpReward) {
+        _id = (short) IdOfKilledMonster;
+        _exp = ExpReward;
+    }
 
     public byte[] getContent() throws IOException, Throwable {
-      byte [] b = {(byte)0xc3 ,0x08 ,0x16 ,(byte)0x9c ,(byte)0x82 ,(byte)0x8a ,(byte)0x46 ,(byte)0xaf ,(byte)0x28};
-    return b;
+        mC3Header(0x16, 0x08); //header
+        writeI(_id);
+        writeC(0x00);//3th bait with exp count
+        writeIDiverted(_exp);
+        return getBytes();
     }
 
     public String getType() {
@@ -25,5 +40,4 @@ public class SGoneExp extends ServerBasePacket{
     public boolean testMe() {
         return true;
     }
-
 }
