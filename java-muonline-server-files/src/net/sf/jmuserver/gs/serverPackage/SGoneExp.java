ed4099de39c048d16833fd068c85c0a306759929
mikiones <mikiones@localhost>
2008-07-30 00:22:49 +0000

    Protocol: ExpAfterKill Packet still someting wrong with data but work fine

--- java-muonline-server-files/src/net/sf/jmuserver/gs/serverPackage/SGoneExp.java
+++ java-muonline-server-files/src/net/sf/jmuserver/gs/serverPackage/SGoneExp.java
@@ -27,13 +27,20 @@ public class SGoneExp extends ServerBasePacket {
 
     public byte[] getContent() throws IOException, Throwable {
         //c3 08 16 
+        //03 60 
+        //59 90 00 06                                                        cc                                                     
+        byte [] t = {(byte)0xc3 ,(byte)0x09 ,(byte)0x16 ,(byte)0x03 ,(byte)0x60 ,(byte)0x59 ,(byte)0x90 ,(byte)0x00 ,(byte)0x06 };
         //03 87 
         //c6 f0 00
-        mC3Header(0x16, 0x08); //header
-        writeC(0x00);
-        writeC(_id);
-        writeC(0x00);//3th bait with exp count
-        writeIDiverted(_exp);
+        mC3Header(0x16, 0x09); //header
+        writeC(0x00);  // 1
+        writeC(_id); // 1
+        writeL(_exp);//4 b
+        //writeC(0x00);//3th bait with exp count
+        //writeC(0x00);
+        //writeC(_exp);
+        //writeC(0x0A);
+        
         return getBytes();
     }
 
