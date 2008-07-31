28c97488126c89f60f64bbb0793170a2b3be9b2d
mikiones <mikiones@localhost>
2008-07-31 11:41:03 +0000

    SendInwentory List after Character stats , some clean

--- java-muonline-server-files/src/net/sf/jmuserver/gs/clientPackage/CA0Request.java
+++ java-muonline-server-files/src/net/sf/jmuserver/gs/clientPackage/CA0Request.java
@@ -16,16 +16,10 @@ public class CA0Request extends ClientBasePacket {
 
     private MuPcInstance cha = null;
     private SSelectedCharacterEnterAnsfer _characterStatsPack = new SSelectedCharacterEnterAnsfer();
-	private SSkillList _skilPack=new SSkillList();
+    private SSkillList _skilPack = new SSkillList();
     private SManaStaminaStats _manaStaminaMaximalsPack = new SManaStaminaStats(SManaStaminaStats._UPDATE_MAX);
-    private SLiveStats _liveMaximalsPack = new SLiveStats(SLiveStats._UPDATE_MAX);
-    private SInwentoryList _inwentoryPack = new SInwentoryList();
-    //private byte[] _itemPack={(byte) 0xc4,0x00,0x06,(byte) 0xf3,0x10,0x00,0x//00};
-    private byte[] _itemPack = {
-        (byte)0xc4 ,(byte)0x00 ,(byte)0x0b ,(byte)0xf3 ,(byte)0x10 ,
-        (byte)0x01 ,//count
-        (byte)0x0c ,//wich slot
-        (byte)0xe3 ,(byte)0x00,(byte)0x00 ,(byte)0x80  };
+    private SLiveStats _liveMaximalsPack = new SLiveStats(SLiveStats._UPDATE_MAX);    //private byte[] _itemPack={(byte) 0xc4,0x00,0x06,(byte) 0xf3,0x10,0x00,0x//00};
+
     //private byte[] _a0ansferPack={(byte) 0xc1,0x05,(byte) 0xa0,0x01,(byte) 0xaa,0x00};
     //private byte[] _learnSkill={(byte) 0xc1 ,0x08 ,(byte) 0xf3 ,0x11 ,(byte) 0xfe ,0x01 ,0x04 ,0x00};
     public CA0Request(byte[] decrypt, ClientThread _client) {
@@ -39,16 +33,16 @@ public class CA0Request extends ClientBasePacket {
         setLiveMaximals();
 
         try {
-            _client.getConnection().sendPacket(new byte[]{(byte)0xc1 ,(byte)0x05 ,(byte)0xa0 ,(byte)0x01 ,(byte)0xaa });
+            _client.getConnection().sendPacket(new byte[]{(byte) 0xc1, (byte) 0x05, (byte) 0xa0, (byte) 0x01, (byte) 0xaa});
             _client.getConnection().sendPacket(_characterStatsPack);
-            _client.getConnection().sendPacket(new SInwentoryList());
+            _client.getConnection().sendPacket(new SInwentoryList(_client.getActiveChar().getInventory()));
             _client.getConnection().sendPacket(_liveMaximalsPack);
             _client.getConnection().sendPacket(_manaStaminaMaximalsPack);
-            
-           // _client.getConnection().sendPacket(_itemPack);
-           // _client.getConnection().sendPacket(_inwentoryPack);
-        //_client.getConnection().sendPacket(_skilPack);
-        _client.getActiveChar().ISpown();
+
+            // _client.getConnection().sendPacket(_itemPack);
+            // _client.getConnection().sendPacket(_inwentoryPack);
+            //_client.getConnection().sendPacket(_skilPack);
+            _client.getActiveChar().ISpown();
         //_client.getConnection().sendPacket(_a0ansferPack);
         //_client.getConnection().sendPacket(_learnSkill);
 
@@ -82,7 +76,7 @@ public class CA0Request extends ClientBasePacket {
 
     private void setSkillList() {
         System.out.println("Skille postaci zaloadowane");
-    
+
     }
 
     @Override
