32cdb3cfb2b9f3dc447377c903df0c165bde8946
mikiones <mikiones@localhost>
2008-07-31 11:42:13 +0000

    Protocol:InwentoryList Now work !!

--- java-muonline-server-files/src/net/sf/jmuserver/gs/serverPackage/SInwentoryList.java
+++ java-muonline-server-files/src/net/sf/jmuserver/gs/serverPackage/SInwentoryList.java
@@ -5,6 +5,9 @@
 package net.sf.jmuserver.gs.serverPackage;
 
 import java.io.IOException;
+import java.util.Collection;
+import net.sf.jmuserver.gs.muObjects.MuCharacterInventory;
+import net.sf.jmuserver.gs.muObjects.MuStoreableItem;
 
 /**
  *
@@ -12,26 +15,36 @@ import java.io.IOException;
  */
 public class SInwentoryList extends ServerBasePacket {
 
-    private byte pac[] = {(byte) 0xc4, (byte) 0x00, (byte) 0x0b, (byte) 0xf3, (byte) 0x10, (byte) 0x01, (byte) 0x0c, (byte) 0xe3, (byte) 0x00, (byte) 0x00, (byte) 0x80, (byte) 0x00, 0x00};
-    private byte pac1[] = {(byte) 0xc3, (byte) 0x0c, (byte) 0x24, (byte) 0x00, (byte) 0x00, (byte) 0xc0, (byte) 0x00, (byte) 0x16, (byte) 0x00, (byte) 0x00, (byte) 0xff, (byte) 0x0c};
+    public final int ItemSize = 5;
+    MuCharacterInventory _inw;
 
-    public SInwentoryList() {
+    public SInwentoryList(MuCharacterInventory inw) {
         super();
-        _bao.write(pac1, 0, pac1.length);
+        _inw = inw;
+
     }
 
-    @Override
+    private void makeHead(int ItemCout) {
+        int size = 6 + (ItemCout * (ItemSize + 1));
+        mC4Header(0xf3, 0x10, size);
+        writeC(ItemCout);
+    }
+
+    private void makeSub(MuStoreableItem item) {
+        int pos = item.getPosition();
+        writeC(pos); //1
+        writeB(item.toByteArray()); // 5
+    //size 6
+    }
+
     public byte[] getContent() throws IOException, Throwable {
-byte t[]={
-    (byte)0xc4 ,(byte)0x00 ,
-    (byte)12 ,(byte)0xf3 ,
-    (byte)0x10 ,(byte)0x01 ,
-    (byte)0x0f ,(byte)0x20 ,
-    (byte)0x00 ,(byte)0x12  ,
-    (byte)0x00 ,(byte)0x00// ,
-    //(byte)0x0c 
-};
-        return t;
+//        byte t[] = {(byte) 0xc4, (byte) 0x00, (byte) 12, (byte) 0xf3, (byte) 0x10, (byte) 0x01, (byte) 0x0f, (byte) 0x20, (byte) 0x00, (byte) 0x12, (byte) 0x00, (byte) 0x00};
+        Collection<MuStoreableItem> inw = _inw.getItems();
+        makeHead(_inw.getCoutofItems());
+        for (MuStoreableItem muStoreableItem : inw) {
+            makeSub(muStoreableItem);
+        }
+        return getBytes();
     }
 
     @Override
