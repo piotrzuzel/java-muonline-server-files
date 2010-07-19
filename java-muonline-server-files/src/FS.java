f6ea4266f7e25dac4d97c5289f8a6d68b9b6125b
mikiones <mikiones@localhost>
2010-07-19 22:36:19 +0000

    Logger setting

--- java-muonline-server-files/src/FS.java
+++ java-muonline-server-files/src/FS.java
@@ -1,5 +1,4 @@
 
-
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -8,61 +7,60 @@ import java.net.Socket;
 import java.util.logging.*;
 import net.sf.jmuserver.fs.FriendTheard;
 
-
-
 public class FS extends Thread {
-	private ServerSocket _serverSocket;
-        public static Logger FSLogger = Logger.getLogger("FriendServer");
-	private String _ip="0";
-	public static int _port=55980;
 
-	public static void main(String[] args) throws Exception {
-		FS server = new FS();
-		FSLogger.info("Friend Server listen on port "+_port);
-		server.start();
-	}
+    private ServerSocket _serverSocket;
+    public static Logger FSLogger = Logger.getLogger("FriendServer");
+    private String _ip = "0";
+    public static int _port = 55980;
 
-	/**
-	 * 
-	 */
-	public void run() {
- 
-            FSLogger.info("Friend Server listen on port "+_port);
-		while (true) {
-			try {
-                        
-				FSLogger.info("used mem:" + getUsedMemoryMB() + "MB");
-				Socket connection = _serverSocket.accept();
-				FriendTheard c= new FriendTheard(connection);
-                          
-                                
-			} catch (IOException e) {
-				// not a real problem
-			}
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        FS server = new FS();
+        FSLogger.info("Friend Server listen on port " + _port);
+        server.start();
+    }
 
-	public long getUsedMemoryMB() {
-		return (Runtime.getRuntime().totalMemory() - Runtime.getRuntime()
-				.freeMemory()) / 1024 / 1024;
-	}
+    /**
+     *
+     */
+    public void run() {
 
-	public FS() throws Exception {
-		super("FS");
+        FSLogger.info("Friend Server listen on port " + _port);
+        while (true) {
+            try {
 
-		FSLogger.info("used mem:" + getUsedMemoryMB() + "MB");
+                FSLogger.info("used mem:" + getUsedMemoryMB() + "MB");
+                Socket connection = _serverSocket.accept();
+                FriendTheard c = new FriendTheard(connection);
 
-		String hostname = "*";
 
-		if (!"*".equals(hostname)) {
-			InetAddress adr = InetAddress.getByName(hostname);
-			_ip = adr.getHostAddress();
-			_serverSocket = new ServerSocket(_port, 50, adr);
-	
-			_serverSocket = new ServerSocket(_port);
-		}
+            } catch (IOException e) {
+                // not a real problem
+            }
+        }
+    }
 
-		int maxPlayers = 10;
+    public long getUsedMemoryMB() {
+        return (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024 / 1024;
+    }
 
-	}
+    public FS() throws Exception {
+        super("FS");
+
+        FSLogger.info("used mem:" + getUsedMemoryMB() + "MB");
+
+        String hostname = "*";
+
+        if (!"*".equals(hostname)) {
+            InetAddress adr = InetAddress.getByName(hostname);
+            _ip = adr.getHostAddress();
+            _serverSocket = new ServerSocket(_port, 50, adr);
+
+        } else {
+            _serverSocket = new ServerSocket(_port);
+        }
+
+        int maxPlayers = 10;
+
+    }
 }
