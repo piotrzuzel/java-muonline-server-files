ee8a18538b1792e2a7e35e62ae1d3058adce1c62
mikiones <mikiones@gmail.com>
2010-08-03 22:04:09 +0100

    small fixes after merge

--- java-muonline-server-files/src/ApppMain.java
+++ java-muonline-server-files/src/ApppMain.java
@@ -14,87 +14,55 @@ import net.sf.jmuserver.logger.MuLoggerMennager;
 public class ApppMain extends Thread {
     //Socket listener
 
-    private ServerSocket _serverSocket;
+	private ServerSocket _serverSocket;
     //Ip adresses
-    private String _ip;
+	private String _ip;
     //Port listener
-    public static int _port;
+	public static int _port;
     //GS Global Logger
     static Logger GSLogger = Logger.getLogger("GameServer");
 
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
         MuLoggerMennager.getInstance();
         GSLogger.info("WorkingDir: " + System.getProperty("user.dir"));
-        ApppMain server = new ApppMain();
+		ApppMain server = new ApppMain();
         FS FServer = new FS();
         GSLogger.info("GameServer Listening on port " + _port);
-        server.start();// runing GS
-        FServer.start();//runing FS
-    }
+		server.start();// runing GS
+                FServer.start();//runing FS
+	}
 
-    public void run() {
-        GSLogger.info("Init Reegions:...");
+	public void run() {
+        GSLogger.info("Init Regions:...");
         MuWorld.getInstance().initWorld();
         GSLogger.info("Init Gates.....");
         MuWorld.getInstance().InitGates();
         CommandHandler.getInstancec();
-        while (true) {
-            try {
-                Socket connection = _serverSocket.accept();
+		while (true) {
+			try {
+				Socket connection = _serverSocket.accept();
                 ClientThread c = new ClientThread(connection);
-            } catch (IOException e) {
-                // not a real problem
-            }
-        }
-    }
+			} catch (IOException e) {
+				// not a real problem
+			}
+		}
+	}
 
-    public long getUsedMemoryMB() {
+	public long getUsedMemoryMB() {
         return (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024 / 1024;
-    }
+	}
 
-    public ApppMain() throws Exception {
-        super("AppMain");
-        GameServerConfig.getInstance();
-        _ip = GameServerConfig.GS_IP;
-        _port = GameServerConfig.GS_PORT;
-
-        GSLogger.info("used mem:" + getUsedMemoryMB() + "MB");
-
-        String hostname = "*";
-
-        if (!"*".equals(hostname)) {
-            InetAddress adr = InetAddress.getByName(hostname);
-            _ip = adr.getHostAddress();
-            _serverSocket = new ServerSocket(_port, 50, adr);
-        } else {
-            _serverSocket = new ServerSocket(_port);
-        }
-        int maxPlayers = 10;
-
-    }
-}
+	public ApppMain() throws Exception {
 		super("AppMain");
-		
-		//SwingUtilities.invokeLater(new Runnable() {
-		//	public void run() {
-		//		
-		//		application.setVisible(true);
-		//	}
-		//});
-		
-		// LogManager.getLogManager().addLogger(_log);
-		// LogOutputStream sl=new OutputStream(new FileOutputStream("log.txt"));
-                
-                // initialize config
-                GameServerConfig.getInstance();
-                
+                  GameServerConfig.getInstance();
+
                 _ip = GameServerConfig.GS_IP;
                 _port = GameServerConfig.GS_PORT;
-                
+
 		System.out.println("used mem:" + getUsedMemoryMB() + "MB");
 
 		String hostname = "*";
-		
+
 		//String port = "port";
 		//_port = 55901;
 
